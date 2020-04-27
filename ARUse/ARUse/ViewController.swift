814ac70d26246a2c17561f63ba0238cddfa962b0
liqiang01 <liqiang01@inke.cn>
2020-04-27 19:54:47 +0800

    MOD: fix ui

---
+++ ARUse/ARUse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARUse
+//
+//  Created by kong on 2020/4/27.
+//  Copyright © 2020 kong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
